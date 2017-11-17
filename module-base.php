c0556cf610c612767d6954d21549c68ea7d10f8c
Seth Whiting <sethcwhiting@gmail.com>
2017-11-17 17:44:40 -0500

    renames wrappers and adds a little styling to one-col text

--- module-base.php
+++ module-base.php
@@ -2,9 +2,9 @@
     <?php while (have_rows('anchour_modules')): the_row(); ?>
 
         <section <?=apply_filters('AML/ModuleAttributes', '')?>>
-            <div class="aml-wrapper-1">
-                <div class="aml-wrapper-2">
-                    <div class="aml-wrapper-3">
+            <div class="crust">
+                <div class="mantle">
+                    <div class="core">
 
                         <?php $layout = str_replace('_', '-', get_row_layout()); ?>
                         <?php include $GLOBALS['aml_path'] . 'module-templates/' . $layout . '.php'; ?>
