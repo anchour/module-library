f3ae164fc3c1fa7a5333f4c291229735c1085b85
Seth Whiting <sethcwhiting@gmail.com>
2017-12-06 16:04:15 -0500

    adds button wrappers

--- module-templates/partials/buttons.php
+++ module-templates/partials/buttons.php
@@ -1,7 +1,9 @@
 <?php if (have_rows('buttons')): ?>
     <div class="am-buttons">
         <?php while (have_rows('buttons')): the_row();?>
-            <a <?=apply_filters('AML/ButtonAttributes', '')?>><?=get_sub_field('button_text')?></a>
+            <div class="am-button-wrap">
+                <a <?=apply_filters('AML/ButtonAttributes', '')?>><?=get_sub_field('button_text')?></a>
+            </div>
         <?php endwhile; ?>
     </div>
 <?php endif; ?>
