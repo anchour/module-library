42de3887a2836cccbd3f4910f585a972d11179d4
Matt Robitaille <matthew@robitaille.io>
2017-12-11 11:20:29 -0500

    Leverage composer autoloading for module library. Namespacing, too.

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+namespace Anchour\ModuleLibrary;
+
 /**
  * Plugin Name: Anchour Module Library
  * Description: Pre-packaged modules for flexible-content fields.
@@ -9,12 +12,17 @@
 $aml_path = plugin_dir_path(__FILE__);
 $partial_path = $aml_path . 'module-templates/partials/';
 
-include 'src/setup.php';
-include 'src/filters.php';
+require __DIR__ . '/vendor/autoload.php';
 
-function includeModules() {
+/**
+ * The base module wrapper that goes around every type of module
+ * within this plugin.
+ *
+ * @return void
+ */
+function includeModules()
+{
     include 'module-base.php';
 }
 
-add_action( 'get_footer', 'includeModules' );
-?>
+add_action('get_footer', __NAMESPACE__ . '\\includeModules');
--- lib/filters.php
+++ lib/filters.php
@@ -1,4 +1,7 @@
 <?php
+
+namespace Anchour\ModuleLibrary;
+
 /**
  * Gets the attributes/classes for each page row in the flexible page layout.
  *
@@ -15,27 +18,51 @@ function aml_module_attributes($attributes): string
     $layout = sanitize_title(str_replace('_', '-', get_row_layout()));
     $classes->push('am-' . $layout);
 
-    $classes->push('am-bg-' . sanitize_title(get_sub_field('background_color') ? : 'white'));
-    $classes->push('am-txt-' . sanitize_title(get_sub_field('text_color') ? : 'primary'));
+    $classes->push('am-bg-' . sanitize_title(get_sub_field('background_color') ?: 'white'));
+    $classes->push('am-txt-' . sanitize_title(get_sub_field('text_color') ?: 'primary'));
 
-    if ($txt_bg = get_sub_field('text_section_background')) $classes->push('am-text-section-bg-' . $txt_bg);
+    if ($txt_bg = get_sub_field('text_section_background')) {
+        $classes->push('am-text-section-bg-' . $txt_bg);
+    }
 
-    if ($width = get_sub_field('content_width')) $classes->push('am-content-width-' . $width);
-    if ($height = get_sub_field('content_height')) $classes->push('am-content-height-' . $height);
+    if ($width = get_sub_field('content_width')) {
+        $classes->push('am-content-width-' . $width);
+    }
+    if ($height = get_sub_field('content_height')) {
+        $classes->push('am-content-height-' . $height);
+    }
 
-    if ($columns = get_sub_field('columns')) $classes->push('am-column-count-' . count($columns));
+    if ($columns = get_sub_field('columns')) {
+        $classes->push('am-column-count-' . count($columns));
+    }
 
-    if ($align_x = get_sub_field('horizontal_alignment')) $classes->push('am-align-x-' . $align_x);
-    if ($align_y = get_sub_field('vertical_alignment')) $classes->push('am-align-y-' . $align_y);
+    if ($align_x = get_sub_field('horizontal_alignment')) {
+        $classes->push('am-align-x-' . $align_x);
+    }
+    if ($align_y = get_sub_field('vertical_alignment')) {
+        $classes->push('am-align-y-' . $align_y);
+    }
 
-    if (get_sub_field('remove_top_padding')) $classes->push('am-padding-top-0');
-    if (get_sub_field('remove_bottom_padding')) $classes->push('am-padding-bottom-0');
+    if (get_sub_field('remove_top_padding')) {
+        $classes->push('am-padding-top-0');
+    }
+    if (get_sub_field('remove_bottom_padding')) {
+        $classes->push('am-padding-bottom-0');
+    }
 
-    if (get_sub_field('image_first')) $classes->push('am-flip-columns');
-    if (get_sub_field('overlapping_columns')) $classes->push('am-column-overlap');
+    if (get_sub_field('image_first')) {
+        $classes->push('am-flip-columns');
+    }
+    if (get_sub_field('overlapping_columns')) {
+        $classes->push('am-column-overlap');
+    }
 
-    if ($placement = get_sub_field('icon_placement')) $classes->push('am-icon-placement-' . $placement);
-    if ($sections = get_sub_field('sections_per_row')) $classes->push('am-sections-per-row-' . $sections);
+    if ($placement = get_sub_field('icon_placement')) {
+        $classes->push('am-icon-placement-' . $placement);
+    }
+    if ($sections = get_sub_field('sections_per_row')) {
+        $classes->push('am-sections-per-row-' . $sections);
+    }
 
     static $count = 0;
     $count++;
@@ -50,14 +77,16 @@ function aml_module_attributes($attributes): string
         $attributes->put('style', 'background-image:url(' . $img . ');');
     }
 
-    if ($id = get_sub_field('id')) $attributes->put('id', $id);
+    if ($id = get_sub_field('id')) {
+        $attributes->put('id', $id);
+    }
 
     return $attributes->map(function ($value, $key) {
         return "$key=\"$value\"";
     })->implode(' ');
 }
 
-add_filter('AML/ModuleAttributes', 'aml_module_attributes');
+add_filter('AML/ModuleAttributes', __NAMESPACE__ . '\\aml_module_attributes');
 
 /**
  * Gets the attributes/classes for each page row in the flexible page layout.
@@ -75,12 +104,14 @@ function aml_button_attributes(): string
     $attributes->put('href', $link);
 
     if (parse_url($link, PHP_URL_HOST) && parse_url($link, PHP_URL_HOST) !== $_SERVER['HTTP_HOST']) {
-        $attributes->put('target', "_blank");
+        $attributes->put('target', '_blank');
     }
 
     $classes = collect(['am-button']);
-    $classes->push('am-button-' . sanitize_title(get_sub_field('button_color') ? : 'primary'));
-    if (get_sub_field('hollow')) $classes->push('am-button-hollow');
+    $classes->push('am-button-' . sanitize_title(get_sub_field('button_color') ?: 'primary'));
+    if (get_sub_field('hollow')) {
+        $classes->push('am-button-hollow');
+    }
 
     $attributes->put('class', $classes->implode(' '));
 
@@ -89,7 +120,7 @@ function aml_button_attributes(): string
     })->implode(' ');
 }
 
-add_filter('AML/ButtonAttributes', 'aml_button_attributes');
+add_filter('AML/ButtonAttributes', __NAMESPACE__ . '\\aml_button_attributes');
 
 function lazyloaded_image_tag(int $imageId = 0, $size = 'large', $attrs = []): string
 {
@@ -97,7 +128,7 @@ function lazyloaded_image_tag(int $imageId = 0, $size = 'large', $attrs = []): s
         return '';
     }
 
-    $file      = get_attached_file($imageId);
+    $file = get_attached_file($imageId);
     $extension = pathinfo($file, PATHINFO_EXTENSION);
 
     if ($extension === 'svg') {
@@ -116,30 +147,28 @@ function lazyloaded_image_tag(int $imageId = 0, $size = 'large', $attrs = []): s
     return $tag;
 }
 
-add_filter('AML/LazyloadedImage', 'lazyloaded_image_tag', 10, 3);
-
+add_filter('AML/LazyloadedImage', __NAMESPACE__ . '\\lazyloaded_image_tag', 10, 3);
 
 function icon_group_content_order(string $placement, int $i): string
 {
-    $fields = array();
+    $fields = [];
     switch ($placement) {
         case 'top':
         case 'left':
-            $fields = array('image', 'header', 'content', 'fake-button');
+            $fields = ['image', 'header', 'content', 'fake-button'];
             break;
 
         case 'bottom':
         case 'right':
-            $fields = array('header', 'content', 'fake-button', 'image');
+            $fields = ['header', 'content', 'fake-button', 'image'];
             break;
 
         case 'middle':
-            $fields = array('header', 'image', 'content', 'fake-button');
+            $fields = ['header', 'image', 'content', 'fake-button'];
             break;
     }
 
     return $fields[$i];
 }
 
-add_filter('AML/IconGroupContentOrder', 'icon_group_content_order', 10, 2);
-?>
+add_filter('AML/IconGroupContentOrder', __NAMESPACE__ . '\\icon_group_content_order', 10, 2);
